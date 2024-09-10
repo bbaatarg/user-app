bdaadcb10c96c6a5237586c7d7b7ecc4e8b350b7
Steller <steller2882@gmail.com>
2024-09-10 13:45:45 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Bayraa!</h1>
+      <p>react test</p>
+    </div>
+  );
+}
+
+export default App;
